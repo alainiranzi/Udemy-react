3a5960193959b29de52def48b8432ed6eb3ad2bd
Alain Iranzi <alainiranzi00@gmail.com>
2025-11-19 13:26:30 +0200

    css styling and css modules

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,12 @@
-
+import classes from './post.module.css';
 
 function Post(props) {
 
 
     return (
-        <div className="git">
-            <p>{props.author}</p>
-            <p>{props.body}</p>
+        <div className="classes.post">
+            <p className={classes.author}>{props.author}</p>
+            <p className={classes.text}>{props.body}</p>
         </div>
     );
 }
