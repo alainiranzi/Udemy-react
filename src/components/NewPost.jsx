83a0cada3a32ca29376d44b3eb17bd06bef8eca2
Alain Iranzi <alainiranzi00@gmail.com>
2025-11-22 10:00:22 +0200

    still on forms

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -5,11 +5,11 @@ function NewPost() {
         <form className={classes.form}>
             <p>
                 <label htmlFor="body">Text</label>
-                <textarea id="body" required rows={3} />
+                <textarea id="body" />
                 </p>
                 <p>
                     <label htmlFor="name">Your name</label>
-                    <input type="text" id="name" required />
+                    <input type="text" id="name" />
                     </p>
                     </form>
     );
