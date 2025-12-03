a567b94d33b6b73bf955ecfe1c8ace285efeb136
Alain Iranzi <alainiranzi00@gmail.com>
2025-12-03 18:30:51 +0200

    state and conditional content

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,15 +1,21 @@
+import classes from './Modal.module.css';
 
-import  classes from './Modal.module.css';
- 
-function  Modal({ children }) {
-    return (
-     <>
-    <div className={classes.backdrop} />
-    <dialog open className={classes.modal}>
-      {children}
-      </dialog>
-         
-    </> 
-    );
+function Modal({ children, onClose }) {
+  return (
+    <>
+      {/* backdrop (lower z-index) */}
+      <div
+        className={classes.backdrop}
+        onClick={onClose}
+        style={{ zIndex: 1000 }}
+      />
+
+      {/* modal container (higher z-index) */}
+      <div className={classes.modal} style={{ zIndex: 1001 }}>
+        {children}
+      </div>
+    </>
+  );
 }
+
 export default Modal;
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -5,8 +5,13 @@ import Modal from './Modal';
 import classes from './PostList.module.css';
 
 function PostList() {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
   const [enteredBody, setEnteredBody] = useState('');
   const [enteredAuthor, setEnteredAuthor] = useState('');
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
 
   function bodyChangeHandler(event) {
     setEnteredBody(event.target.value);
@@ -16,14 +21,18 @@ function PostList() {
     setEnteredAuthor(event.target.value);
   }
 
-  return (
-    <>
-      <Modal>
-        <NewPost 
-          onBodyChange={bodyChangeHandler} 
-          onAuthorChange={authorChangeHandler} 
+  
+     return (
+      <>
+      {modalIsVisible && (
+     <Modal onClose={hideModalHandler}>
+        <NewPost
+          onBodyChange={bodyChangeHandler}
+          onAuthorChange={authorChangeHandler}
         />
       </Modal>
+    )}
+  
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Alain" body="Check out the full course!" />
